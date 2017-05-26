32bed3df2324f8f468a3e9d73e2e6880b983482a
coremessage <cowmessage@gmail.com>
2017-05-26 15:17:24 +0300

    Add 'gems' option

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ module.exports = function(source) {
   const dontIncludeOpal = typeof this.query === 'string'
     ? /(\?|&)includeOpal=false/.test(this.query)
     : (this.query.includeOpal === false || this.query.includeOpal === 'false');
-  const cmd = `opal -c '${this.resourcePath}' -I '${this.context}'${dontIncludeOpal ? ' --no-opal' : ''}`;
+  const gems = this.query.gems && this.query.gems.map(x => `-g ${x}`).join(' ');
+  const cmd = `opal -c '${this.resourcePath}' -I '${this.context}'${dontIncludeOpal ? ' --no-opal' : ''} ${gems}`;
 
   exec(cmd, { maxBuffer: 1024 * 5000 }, function (error, stdout, stderr) {
     if (error) { return callback(error, null); }
